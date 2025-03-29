70dda1f0ea22ac3848ec2220e27e1d32db008b83
jnesss <jness@fusions.ai>
2025-03-29 10:13:00 -0700

    no go generate for bpf on mac os

--- bpf_darwin.go
+++ bpf_darwin.go
@@ -15,3 +15,5 @@ func InitBPF() (PerfReader, func(), error) {
 	fmt.Println("BPF monitoring not available on MacOS. Starting in web-only mode...")
 	return nil, nil, nil
 }
+
+// No need for go:generate since we don't compile BPF code on macOS
