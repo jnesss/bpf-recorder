0df7799517a5cc1d5ad40e7f135afc7470226192
jnesss <jness@fusions.ai>
2025-03-31 10:31:53 -0700

    use md5 for initial implementation to track whether we have a copy of the binary or not

---
+++ binary_hash.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"io"
+	"os"
+)
+
+// calculateMD5 generates an MD5 hash of a file
+func calculateMD5(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, file); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(hash.Sum(nil)), nil
+}
