7c119c5e5608f35f04390d1f8d12af2549df532a
jnesss <jness@fusions.ai>
2025-04-02 16:18:33 -0700

    remove build directives out of main.go

--- main.go
+++ main.go
@@ -1,6 +1,3 @@
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g -Wall -Werror" netmonBPF bpf/netmon.c
-//go:generate go run github.com/cilium/ebpf/cmd/bpf2go -cc clang -cflags "-O2 -g -Wall -Werror" execveBPF bpf/execve.c
-
 package main
 
 import (
